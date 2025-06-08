86b36cee171c45e8eb1532bc2dbe3d6bdb96fe8f
Pratik <93414233+pratik-git10@users.noreply.github.com>
2025-06-08 23:23:52 +0530

    removed pageloader

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,19 @@ import Blog from "./pages/blog";
 import Terms from "./pages/terms";
 import Privacy from "./pages/privacy";
 import Footer from "./components/usable/footer";
-import PageLoader from "./components/usable/PageLoader";
-// import { Home } from "@/pages/Home.";
 
 const App = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <div className="flex-1">
         <Router>
-          <PageLoader>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/blog" element={<Blog />} />
-              <Route path="/terms" element={<Terms />} />
-              <Route path="/privacy-policy" element={<Privacy />} />
-            </Routes>
-          </PageLoader>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/terms" element={<Terms />} />
+            <Route path="/privacy-policy" element={<Privacy />} />
+          </Routes>
         </Router>
       </div>
       <Footer />
--- src/pages/terms.tsx
+++ src/pages/terms.tsx
@@ -22,6 +22,16 @@ const Terms = () => {
       paragraph:
         "Users must use the platform responsibly and in compliance with applicable laws. Prohibited behavior includes unauthorized access, data scraping, and posting harmful content.",
     },
+    {
+      heading: "User Conduct",
+      paragraph:
+        "Users must use the platform responsibly and in compliance with applicable laws. Prohibited behavior includes unauthorized access, data scraping, and posting harmful content.",
+    },
+    {
+      heading: "User Conduct",
+      paragraph:
+        "Users must use the platform responsibly and in compliance with applicable laws. Prohibited behavior includes unauthorized access, data scraping, and posting harmful content.",
+    },
   ];
 
   // Simulate loading
@@ -34,7 +44,7 @@ const Terms = () => {
     <div className="">
       <NavLink
         to={"/"}
-        className="flex gap-2 items-center hover:bg-neutral-800 px-2 py-1 rounded-md w-fit m-2 md:m-10 transition transform ease-in-out duration-200 mt-4 md:mt-20">
+        className="flex gap-2 items-center hover:bg-neutral-800 px-2 py-1 rounded-md w-fit m-2 md:m-10 transition transform ease-in-out duration-200 mt-4 md:mt-20 sticky top-3 ">
         <ArrowLeft />
         <h1>Back</h1>
       </NavLink>
